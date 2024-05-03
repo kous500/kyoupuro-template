e3e6dd22d7cd31ff13c5d50da1cc09db89d897bf
kous500 <so5000ap@gmail.com>
2024-05-03 13:04:27 +0900

    座標圧縮を追加

--- template.cpp
+++ template.cpp
@@ -129,6 +129,17 @@ int lower_binary_search(vector<int> a, int key) {
     return ok;
 }
 
+template<class T> vector<T> coordinate_compression(vector<T>& a) {
+    vector<T> b = a;
+    sort(b.begin(), b.end());
+    b.erase(unique(b.begin(), b.end()), b.end());
+    vector<T> res(a.size());
+    for (int i = 0; i < (int)a.size(); i++) {
+        res[i] = lower_bound(b.begin(), b.end(), a[i]) - b.begin();
+    }
+    return res;
+}
+
 int gcd(int a, int b) {
     if (b == 0) return a;
     else return gcd(b, a % b);
